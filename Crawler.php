c8d6578002cdb14d2a1dc3836811c88342b098be
WATANABEK <38969200+WataKN@users.noreply.github.com>
2018-05-18 22:29:47 +0900

    Add files via upload

---
+++ Crawler.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>クローラ</title>
+    <link rel='rel="canonical"' />
+</head>
+
+<?php
+
+require_once('phpQuery-onefile.php');
+
+// Get Data Source
+$html = file_get_contents("https://no1s.biz/");
+
+// Get DOM Object
+$phpQueryObj = phpQuery::newDocument($html);
+
+$result = '';
+
+// タグごとに取得
+$titleArr = $phpQueryObj['h1'];
+$ulArr = $phpQueryObj['li'];
+
+foreach($titleArr as $val) {
+
+	$title = pq($val)->find('a')->text();
+	$url = pq($val)->find('a')->attr('href');
+
+	$result .= $url .'　'. $title .'<br/>'. PHP_EOL;
+
+}
+
+
+foreach($ulArr as $val) {
+// var_dump($ulArr);
+	$title = pq($val)->find('a')->text();
+	$url = pq($val)->find('a')->attr('href');
+
+	$result .= $url .'　'. $title .'<br/>'. PHP_EOL;
+
+}
+
+echo $result;
+echo PHP_EOL.PHP_EOL;
+
+?>
+
+</html>
